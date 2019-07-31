97a307946ba1c44ca86589d6bfd3f7a75e873643
thecakeisfalse <48329968+thecakeisfalse@users.noreply.github.com>
2019-07-31 20:54:12 +0300

    Update brainfuck.c

--- code/brainfuck.c
+++ code/brainfuck.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char* read_str(size_t *size){
+char* read_string(size_t *size){
     char *str;
     int c;
-    size_t len = 0;
+    size_t length = 0;
     str = realloc(NULL, sizeof(char)*(*size));
     if(!str)return str;
     while((c=getchar()) != EOF  &&  c != '\n'){
-        str[len++] = c;
-        if(len == *size) {
+        str[length++] = c;
+        if(length == *size) {
         	str = realloc(str, sizeof(char)*(*size += 10));
             if(!str)
             	return str;
         }
     }
-    str[len++] = '\0';
-    size = len;
+    str[length++] = '\0';
+    size = length;
     return str;
 }
 
@@ -28,52 +28,52 @@ int main() {
 	for (int k = 0; k < n; k++)
 		tape[k] = 0;
 	int pointer = 0;
-	size_t len = 100;
-	char *code = read_str(&len);
-	printf("%s\n", code);
-	for (unsigned int i = 0; i < len; i++) {
-		if (code[i] == '>')
+	size_t length = 100;
+	char *program = read_string(&length);
+	printf("%s\n", program);
+	for (unsigned int i = 0; i < length; i++) {
+		if (program[i] == '>')
 			pointer++;
 		
-		if (code[i] == '<')
+		if (program[i] == '<')
 			pointer--;
 		
-		if (code[i] == '+')
+		if (program[i] == '+')
 			tape[pointer]++;
 		
-		if (code[i] == '-')
+		if (program[i] == '-')
 			tape[pointer]--;
 		
-		if (code[i] == '.')
+		if (program[i] == '.')
 			printf("%c", tape[pointer]);
 		
-		if (code[i] == ',')
+		if (program[i] == ',')
 			scanf("%c", &tape[pointer]);
 
-		if (code[i] == '[') {
+		if (program[i] == '[') {
 			if (tape[pointer] == 0) {
-				int count = 1;
-				while (count > 0) {
+				int counter = 1;
+				while (counter > 0) {
 					i++;
-					if (code[i] == '[')
-						count++;
-					if (code[i] == ']')
-						count--;
+					if (program[i] == '[')
+						counter++;
+					if (program[i] == ']')
+						counter--;
 				}
 			}
-        }
-        else if (code[i] == ']') {
+                 }
+                 else if (program[i] == ']') {
 			if (tape[pointer] != 0) {
-				int count = 1;
-				while (count > 0) {
+				int counter = 1;
+				while (counter > 0) {
 					i--;
-					if (code[i] == '[')
-						count--;
-					if (code[i] == ']')
-						count++;
+					if (program[i] == '[')
+						counter--;
+					if (program[i] == ']')
+						counter++;
 				}
 			}
-    }
-  }
+                 }
+        }
 	return 0;
 }
