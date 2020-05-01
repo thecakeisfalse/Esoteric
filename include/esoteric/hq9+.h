395dfe1ccc22f615886f29e9280c3a81434ca75e
Simon Chaykin <52384223+game-lover@users.noreply.github.com>
2020-05-01 17:30:34 +0300

    Add files via upload

---
+++ include/esoteric/hq9+.h
@@ -0,0 +1,17 @@
+#ifndef ESOTERIC_HQ9_H
+#define ESOTERIC_HQ9_H 1
+
+#include <stdio.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void hq9(char* commands);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ include/esoteric/malbolge.h
@@ -0,0 +1,19 @@
+#ifndef ESOTERIC_MALBOLGE_H
+#define ESOTERIC_MALBOLGE_H 1
+
+#include <lib/utils.h>
+
+#define POW9 19683
+#define POW10 59049
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void malbolge(char* commands);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
