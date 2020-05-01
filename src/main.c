6c0e0bdacc48e3bb8b163b24c559e33936ae8342
Simon Chaykin <52384223+game-lover@users.noreply.github.com>
2020-05-01 17:47:48 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,23 +4,29 @@
 #include <esoteric.h>
 
 int main(int argc, char* argv[]) {
-	char** arguments = args(argc, argv);
-	char* buf = (char*)malloc(1024);
-	if (arguments[0] == "") {
-		scanf("%s\n", buf);
-	} 
-	else {
+char** arguments = args(argc, argv);
+	char* buf = (char*)malloc(1024), *tmp = (char*)malloc(512);
+	memset(buf, 0, 1024);
+	if (strcmp(arguments[1], "") == 0) {
+		printf("lang> ");
+		scanf("%s", tmp);
+		arguments[1] = tmp;
+	}
+	if (strcmp(arguments[0], "") == 0) {
+		printf("%s> ", arguments[1]);
+		scanf("%s", buf);
+	} else {
 		FILE* file;
 		char c;
 		file = fopen(arguments[0], "r");
-		memset(buf, 0, 1024);
 		int pointer = 0;
 		while ((c = fgetc(file)) != EOF)
 			buf[pointer++] = c;
 		fclose(file);
 	}
 	printf("%s\n", buf);
-	exec(arguments[2], buf);
+	exec(arguments[1], buf);
+	free(tmp);
 	free(buf);
 	return 0;
 }
