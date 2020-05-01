a5377ec70f713f7464b98e19e5d6132eeca71644
Simon Chaykin <52384223+game-lover@users.noreply.github.com>
2020-05-01 13:54:53 +0300

    Create brainfuck.c

---
+++ src/esoteric/brainfuck.c
@@ -0,0 +1,48 @@
+/*
+ * Brainfuck module source file
+ */
+
+#include <esoteric/brainfuck.h>
+
+void brainfuck(char* commands) {
+	char buf[1024];
+	int pointer = 0;
+	memset(buf, 0, 1024*sizeof(char));
+	for (int i = 0; i < strlen(commands); i++) {
+		if (commands[i] == '>') {
+			if (pointer < 1024)
+				pointer++;
+		} else if (commands[i] == '<') {
+			if (pointer > 0)
+				pointer--;
+		} else if (commands[i] == '+') {
+			if (buf[pointer] < 127)
+				buf[pointer]++;
+		} else if (commands[i] == '-') {
+			if (buf[pointer] > 0)
+				buf[pointer]--;
+		} else if (commands[i] == '.') {
+			printf("%c", buf[pointer]);
+		} else if (commands[i] == ',') {
+			scanf("%d", buf[pointer]);
+		} else if (commands[i] == '[') {
+			if (buf[pointer] == 0) {
+				int counter = 1;
+				while (counter > 0) {
+					i++;
+					if (commands[i] == '[') counter++;
+					if (commands[i] == ']') counter--;
+				}
+			}
+		} else if (commands[i] == ']') {
+			if (buf[pointer] != 0) {
+				int counter = 1;
+				while (counter > 0) {
+					i--;
+					if (commands[i] == '[') counter--;
+					if (commands[i] == ']') counter++;
+				}
+			}
+		}
+	}
+}
