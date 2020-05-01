e42269803def103d488587f364f7275cd3b61300
Simon Chaykin <52384223+game-lover@users.noreply.github.com>
2020-05-01 17:28:07 +0300

    Create spoon.c

---
+++ src/esoteric/spoon.c
@@ -0,0 +1,48 @@
+#include <esoteric/spoon.h>
+
+char* spoon2bf(char* commands) {
+	char* buf = malloc(sizeof(char)*strlen(commands));
+	for (int j = 0, i = 0; i < strlen(commands); i++) {
+		if (commands[i] == '1') {
+			buf[j++] = '+';
+		} else if (commands[i] == '0') {
+			i++;
+			if (commands[i] == '0') {
+				i++;
+				if (commands[i] == '0') {
+					buf[j++] = '-';
+				} else if (commands[i] == '1') {
+					i++;
+					if (commands[i] == '0') {
+						i++;
+						if (commands[i] == '0') {
+							buf[j++] = '[';
+						} else if (commands[i] == '1') {
+							i++;
+							if (commands[i] == '0') {
+								buf[j++] = '.';
+							} else if (commands[i] == '1') {
+								buf[j++] = ',';
+							}
+						}
+					} else if (commands[i] == '1') {
+						buf[j++] = ']';
+					}
+				}
+			} else if (commands[i] == '1') {
+				i++;
+				if (commands[i] == '0') {
+					buf[j++] = '>';
+				} else if (commands[i] == '1') {
+					buf[j++] = '<';
+				}
+			}
+		}
+	}
+	return buf;
+}
+
+void spoon(char* commands) {
+	char* buf = spoon2bf(commands);
+	brainfuck(buf);
+}
