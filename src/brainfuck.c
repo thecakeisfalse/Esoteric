edf960f62391fde203af9139580225113d580fff
Simon Chaykin <52384223+game-lover@users.noreply.github.com>
2020-08-17 21:35:17 +0100

    Update brainfuck.c

--- src/brainfuck.c
+++ src/brainfuck.c
@@ -70,28 +70,28 @@ void brainfuck(char *commands) {
  * @param file Contain file pointer for output file.
  */
 void brainfuck_to_c(char *commands, FILE *file) {
-	const char *p = commands;
-	fprintf(file, "#include <stdio.h>\n");
-	fprintf(file, "int main() {\n");
-	fprintf(file, "\tint buffer[30000];\n");
-	fprintf(file, "\tint pointer = 0;\n");
-	do {
-		if (*p == '+')
-			fprintf(file, "\tbuffer[pointer]++;\n");
-		if (*p == '-')
-			fprintf(file, "\tbuffer[pointer]--;\n");
-		if (*p == '>')
-			fprintf(file, "\tpointer++;\n");
-		if (*p == '<')
-			fprintf(file, "\tpointer--;\n");
-		if (*p == '.')
-			fprintf(file, "\tputchar(buffer[pointer]);\n");
-		if (*p == ',')
-			fprintf(file, "\tscanf(\"%%d\", &buffer[pointer]);\n");
-		if (*p == '[')
-			fprintf(file, "\twhile(buffer[pointer]) {\n");
-		if (*p == ']')
-			fprintf(file, "\t}\n");
-	} while (*(p++));
-	fprintf(file, "}");
+  const char *p = commands;
+  fprintf(file, "#include <stdio.h>\n");
+  fprintf(file, "int main() {\n");
+  fprintf(file, "\tint buffer[30000];\n");
+  fprintf(file, "\tint pointer = 0;\n");
+  do {
+    if (*p == '+')
+      fprintf(file, "\tbuffer[pointer]++;\n");
+    if (*p == '-')
+      fprintf(file, "\tbuffer[pointer]--;\n");
+    if (*p == '>')
+      fprintf(file, "\tpointer++;\n");
+    if (*p == '<')
+      fprintf(file, "\tpointer--;\n");
+    if (*p == '.')
+      fprintf(file, "\tputchar(buffer[pointer]);\n");
+    if (*p == ',')
+      fprintf(file, "\tscanf(\"%%d\", &buffer[pointer]);\n");
+    if (*p == '[')
+      fprintf(file, "\twhile(buffer[pointer]) {\n");
+    if (*p == ']')
+      fprintf(file, "\t}\n");
+  } while (*(p++));
+  fprintf(file, "}");
 }
